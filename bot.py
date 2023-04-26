6e2f3ba19f460d3868f85dd402071bd9ca23b77a
Morteza Piralilou <mortazap@yahoo.com>
2023-04-26 03:17:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,11 +25,12 @@ APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 SESSION = config("SESSION")
 FROM_ = config("FROM_CHANNEL")
+FROMTRAN_ = config("FROM_TRAN")
 TO_ = config("TO_CHANNEL")
 
 FROM = [int(i) for i in FROM_.split()]
 TO = [int(i) for i in TO_.split()]
-
+FROMTRAN = [int(i) for i in FROMTRAN_.split()]
 try:
     BotzHubUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
     BotzHubUser.start()
@@ -39,12 +40,21 @@ except Exception as ap:
 
 @BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
 async def sender_bH(event):
-    for i in TO:
+    for i in FROMTRAN:
         try:
             await BotzHubUser.send_message(
                 i,
                 event.message
             )
+            for i in TO:
+                try:
+                    await BotzHubUser.send_message(
+                        i,
+                        event.message
+                )
+                    break
+                except Exception as e:
+                    print(e)
         except Exception as e:
             print(e)
 
